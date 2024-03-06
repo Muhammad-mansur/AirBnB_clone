c6e5d7765b09cdcf421a055ad82cf9c426f8b57c
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-06 18:19:32 +0530

    task 4 - create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,20 @@ class BaseModel:
     """
     Defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initialising
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in['created_at', 'updated_at']:
+                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:            
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
         
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
