8986b9870733b7d64492afe9a3c0cd544274c04d
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-06 04:28:01 -0800

    task 3 fix: datetime error

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """ Base Class """
 
 import datetime
+import models
 import uuid
 
 
@@ -15,7 +16,7 @@ class BaseModel:
         initialising
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
         
     def __str__(self):
@@ -27,7 +28,7 @@ class BaseModel:
         updates the public instance attribute 'updated_at'
         with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
         
     def to_dict(self):
         """
