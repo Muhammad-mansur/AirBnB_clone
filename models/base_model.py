bb6c86f3f3ce639d1210b38641eb1a5e73e872ac
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-06 17:46:05 +0530

    task 3: base model

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+""" Base Class """
+
+import datetime
+import uuid
+
+
+class BaseModel:
+    """
+    Defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        """
+        initialising
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+        
+    def save(self):
+        """
+        updates the public instance attribute 'updated_at'
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values 
+        of __dict__ of the instance
+        
+        Creates a dictionary representation of the object
+        
+        Add the class name to the dictionary
+        
+        convert to string object in ISO format
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
