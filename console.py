0162fce08da42c3b9c758c9fd6069fd90d3ce72f
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-10 01:29:02 +0530

    task 7

--- console.py
+++ console.py
@@ -39,7 +39,7 @@ class HBNBCommand(cmd.Cmd):
             new_instance = BaseModel()
             new_instance.save()
             print(new_instance.id)
-            
+
     def do_show(self, arg):
         """Prints the string rep of an instance"""
         if not arg:
@@ -54,7 +54,7 @@ class HBNBCommand(cmd.Cmd):
                 print(storage.all()[key])
             else:
                 print("** no instance found **")
-                
+
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
         if not arg:
@@ -70,6 +70,51 @@ class HBNBCommand(cmd.Cmd):
             else:
                 del storage.all()[key]
                 storage.save()
+
+    def do_all(self, arg):
+        """Prints all string rep of all instances"""
+        instances = []
+        if not arg:
+            if arg not in ["BaseModel"]:
+                print("** class doesn't exist **")
+                return
+            for key, obj in storage.all().items():
+                if arg in key:
+                    instances.append(str(obj))
+        else:
+            for obj in storage.all().values():
+                instances.append(str(obj))
+
+        print(instances)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split(" ")
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        instance = storage.all()[key]
+        setattr(instance, args[2], args[3])
+        instance.save()
 
 
 if __name__ == '__main__':
