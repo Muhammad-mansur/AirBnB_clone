67c315583fe8df53919f9dd1e6ef857d6871aa1f
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-10 02:16:17 +0530

    task 8 fix

--- models/user.py
+++ models/user.py
@@ -7,11 +7,8 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ A class user that inherits from BaseModel """
-    def __init__(self, *args, **kwargs):
-        """ Initialize user class """
-        super().__init__(args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-        
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
