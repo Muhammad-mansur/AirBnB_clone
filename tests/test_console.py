2473f0a148c69a78327d52cf94a4b3dec4b41c0c
MrSavvy1 <destydesty112@gmail.com>
2024-03-12 15:20:50 -0700

    rhythm

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,8 +3,9 @@
 """ Unittest """
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call, ANY
 from models import storage
+from models.user import User
 from console import HBNBCommand
 from io import StringIO  # For simulating user input
 
@@ -14,8 +15,8 @@ class TestHBNBCommand(unittest.TestCase):
     @patch('sys.stdout')
     def test_quit(self, mock_stdout):
         command = HBNBCommand()
-        self.assertTrue(command.do_quit(""))
-        self.assertEqual(mock_stdout.write.call_count, 1)
+        command.do_quit('')
+        self.assertEqual(mock_stdout.write.call_count, 0)
 
     def test_EOF(self, mock_stdout):
         """ Test End of File """
@@ -35,7 +36,7 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
             command.do_create("User")
             mock_stdout.getvalue().strip()
-        mock_new.assert_called_once_with()
+        mock_new.assert_called_once_with(ANY)
         storage.save.assert_called_once()
 
     @patch('sys.stdout')
@@ -43,16 +44,18 @@ class TestHBNBCommand(unittest.TestCase):
         """ Test invalid class creation """
         command = HBNBCommand()
         command.do_create("InvalidClass")
-        mock_stdout.write.assert_called_once_with(
-                "** class doesn't exist **\n")
+        expected_calls = [call("** class doesn't exist **"), call('\n')]
+        mock_stdout.write.assert_has_call(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
 
     @patch('sys.stdout')
     def test_create_no_class(self, mock_stdout):
         """ Test no class creation """
         command = HBNBCommand()
         command.do_create("")
-        mock_stdout.write.assert_called_once_with(
-                "** class name missing **\n")
+        expected_calls = [call("** class namw missing **\n"), call('\n')]
+        mock_stdout.write.assert_called_has_calls(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
 
     @patch('sys.stdout')
     def test_show_valid_args(self, mock_stdout):
@@ -69,31 +72,36 @@ class TestHBNBCommand(unittest.TestCase):
         """ Test invalid class """
         command = HBNBCommand()
         command.do_show("InvalidClass 1")
-        mock_stdout.write.assert_called_once_with(
-                "** class doesn't exist **\n")
+        expected_calls = [call("** class doesn't exist **"), call('\n')]
+        mock_stdout.write.assert_has_calls(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
 
     @patch('sys.stdout')
     def test_show_no_class(self, mock_stdout):
         """ Test no class """
         command = HBNBCommand()
         command.do_show("")
-        mock_stdout.write.assert_called_once_with(
-                "** class name missing **\n")
+        expected_calls = [call("** class name missing **"), call('\n')]
+        mock_stdout.write.assert_has_calls(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
 
     @patch('sys.stdout')
     def test_show_no_id(self, mock_stdout):
         """ Test no id """
         command = HBNBCommand()
         command.do_show("User")
-        mock_stdout.write.assert_called_once_with(
-                "** instance id missing **\n")
+        expected_calls = [call("** instance id missing **"), call('\n')]
+        mock_stdout.write.assert_has_calls(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
 
     @patch('sys.stdout')
     def test_show_no_instance(self, mock_stdout):
         """ Test no instance """
         command = HBNBCommand()
         command.do_show("User 123")
-        mock_stdout.write.assert_called_once_with("** no instance found **\n")
+        expected_calls = [call("** no instance found **"), call('\n')]
+        mock_stdout.write.assert_has_calls(expected_calls)
+        self.assertEqual(mock_stdout.write.call_count, 2)
         storage.reset()
 
     @patch('sys.stdout')
